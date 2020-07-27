40960314f8ab58f1226b6b123ab754f7cbc512d6
MarilenaRoque <marilenas@id.uff.br>
2020-07-27 13:05:27 -0300

    Added Anagram Answer

--- solutions.rb
+++ solutions.rb
@@ -13,3 +13,24 @@ end
 # FizzBuzz
 
 1.upto(100){|i|s="FizzBuzz";case when i%15==0;when i%5==0;s=s[4..7];when i%3==0; s=s[0..3];else s=i;end;puts s}
+
+# Anagram
+# https://www.hackerrank.com/contests/microverse-coding-challenges/challenges/anagram/problem
+
+def anagram(s)
+  if s.length.odd?
+      return -1 
+  else
+      s1=s.slice(0..s.length/2-1).split("")
+      s2=s.slice(s.length/2..s.length).split("")
+      s1.each do |s1_el| 
+          s2.each_with_index do |s2_el, s2_i| 
+              if s2_el == s1_el       
+                  s2.delete_at(s2_i)
+                  break
+              end
+          end
+      end
+      return s2.length
+  end 
+end
