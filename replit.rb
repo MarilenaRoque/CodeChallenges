0dc4f0c4e494638764ebc62b7d072af5c94cb3f3
MarilenaRoque <marilenas@id.uff.br>
2020-08-03 23:31:56 -0300

    Added #add_at and #remove methods
    Builded add_at to insert a node on a certain index
    Builded remove method to remove a node given its index

--- replit.rb
+++ replit.rb
@@ -8,15 +8,6 @@ class Node
   end
 end
 
-class Node
-  attr_accessor :value, :next_node
-  
-  def initialize(value, next_node = nil)
-	  @value = value
-    @next_node = next_node
-  end
-end
-
 class LinkedList
   def initialize
     @tail = nil
@@ -38,6 +29,37 @@ class LinkedList
     return node.value
   end
   
+  def add_at(index, value)
+    new_node = Node.new(value)
+    if index == 0
+      if @head.nil?
+        @tail = new_node
+      else
+        new_node.next_node = @head
+      end
+      @head = new_node
+    else
+      prev_node = get_node(index - 1)
+      if prev_node.next_node.nil?
+        @tail = new_node
+      else
+        new_node.next_node = prev_node.next_node
+      end
+      prev_node.next_node = new_node
+    end
+  end
+  
+  def remove(index)
+    if index == 0
+      @head = @head.next_node
+    else
+      prev_node = get_node(index - 1)
+      deleted_node = prev_node.next_node
+      prev_node.next_node = deleted_node.next_node
+      @tail = prev_node if prev_node.next_node.nil?
+    end
+  end
+  
   private
   
   def get_node(index)
@@ -53,5 +75,11 @@ list = LinkedList.new
 
 list.add(3)
 list.add(5)
-puts list.get(1)
+list.add_at(1, 11)
+list.add_at(0, 13)
+
+puts list.get(2)
+# => 11
+
+puts list.get(3)
 # => 5
