66105d7c1f418a2b7fa4b5c37f05d09a54f7477f
MarilenaRoque <marilenas@id.uff.br>
2020-09-07 14:57:19 -0300

    Fix #insertFirst and built #printValues

--- Deque.rb
+++ Deque.rb
@@ -22,6 +22,7 @@ class Deque
         if @head.nil?
             @tail = new_node
         else
+            new_node.next_node = @head
             @head.prev_node = new_node
         end
         @head = new_node
@@ -37,5 +38,58 @@ class Deque
         @tail = new_node
     end
 
-end
+    def deleteFirst
+        if head.nil?
+            puts "Error: Deque empty"
+        elsif @head.next_node.nil?
+            @head = nil
+            @tail = nil
+        else
+            @head.next_node.prev_node = nil
+            @head = @head.next_node
+        end
+    end
 
+    def printValues
+        compare = @head
+        while !compare.nil?
+            puts compare.value
+            compare = compare.next_node
+        end
+    end
+
+end
+#Insert Front
+#Insert Last
+#Delete Front
+#Delete Last
+
+#get Front
+#get Rear
+#is Empty
+#is Full
+
+deq = Deque.new
+deq.insertFirst(20)
+deq.insertFirst(5000)
+deq.insertLast(50)
+deq.insertLast(150)
+
+# deq.printValues
+# deq.deleteFirst
+# deq.printValues
+# puts "I am the new head #{deq.head.value}"
+# deq.deleteFirst
+# deq.printValues
+# puts "I am the new head #{deq.head.value}"
+# deq.deleteFirst
+# deq.printValues
+# puts "I am the new head #{deq.head.value}"
+# deq.deleteFirst
+# deq.printValues
+# puts "I am the new head #{deq.head.value}"
+
+
+# puts "I am the last value #{deq.tail.value}"
+# puts "I am the node #{deq.tail}"
+# puts "I am the next value of the head #{deq.head.next_node.value}"
