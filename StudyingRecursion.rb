909204b5fd6a96237714d1fe84882774aab2e38f
MarilenaRoque <marilenas@id.uff.br>
2020-09-28 13:24:28 -0300

    Built bottles function using recursiveness

--- StudyingRecursion.rb
+++ StudyingRecursion.rb
@@ -44,3 +44,14 @@ def palindromeRecursive(s)
         end
     end
 end
+
+def bottles(n)
+    if n == 0
+        puts "no more bottles of beer on the wall"
+    else
+        puts "#{n} bottles of beer on the wall"
+        bottles(n-1)
+    end
+end
+
+bottles(20)
