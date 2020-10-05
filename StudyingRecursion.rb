13e53fb3fb82a184d1e74ce7cbd92ff280100e38
MarilenaRoque <marilenas@id.uff.br>
2020-10-05 12:31:58 -0300

    Undestanding Roman_mapping quiz solution

--- StudyingRecursion.rb
+++ StudyingRecursion.rb
@@ -66,3 +66,60 @@ def flattening(arr, resul=[])
     end
     resul
 end
+
+
+def integer_to_roman(roman_mapping, number, result = "")
+    return result if number == 0
+    roman_mapping.keys.each do |div|
+        quotient, modulus = number.divmod(div)
+        result << roman_mapping[div] * quotient
+        return integer_to_roman(roman_mapping, modulus, result) if quotient > 0
+    end
+end
+
+roman_mapping = {
+  1000 => "M",
+  900 => "CM",
+  500 => "D",
+  400 => "CD",
+  100 => "C",
+  90 => "XC",
+  50 => "L",
+  40 => "XL",
+  10 => "X",
+  9 => "IX",
+  5 => "V",
+  4 => "IV",
+  1 => "I"
+}
+
+
+
+roman_mapping_integer = {
+  "M" => 1000,
+  "CM" => 900,
+  "D" => 500,
+  "CD" => 400,
+  "C" => 100,
+  "XC" => 90,
+  "L" => 50,
+  "XL" => 40,
+  "X" => 10,
+  "IX" => 9,
+  "V" => 5,
+  "IV" => 4,
+  "I" => 1
+}
+
+def roman_to_integer(roman_mapping, str, result = 0)
+    return result if str.empty?
+    roman_mapping.keys.each do |roman|
+        if str.start_with?(roman)
+            result = result + roman_mapping[roman]
+            str = str.slice(roman.length, str.length)
+            return roman_to_integer(roman_mapping, str, result)
+        end 
+    end
+end
+
+puts roman_to_integer(roman_mapping_integer, "MMMXXXIV")
