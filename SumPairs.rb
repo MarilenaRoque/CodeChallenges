5579683fd1fb7e93eb9ac4604e5810e52617e98e
MarilenaRoque <marilenas@id.uff.br>
2020-11-17 12:24:07 -0300

    Initial implementation of BS - not working

--- SumPairs.rb
+++ SumPairs.rb
@@ -4,21 +4,30 @@ def two_sum(nums, target)
     nums.each_with_index do |item, idx|
         arr << [item, idx]
     end
-    find = false
     sorted = arr.sort { |x,y| x[0] <=> y[0] }
-    sorted.each_with_index do |el, first_index|
-        (first_index).upto(nums.length - 1) do |sec_index|
-            if el[0] + sorted[sec_index][0] == target && first_index != sec_index
-              result << el[1]
-              result << sorted[sec_index][1]
-              find = true
-              break
+    sorted.each_with_index do |el, idx_first|
+        start_range = idx_first + 1
+        end_range = sorted.length - 1
+        find = target - el[0]
+
+        while end_range - start_range > 1 do
+
+            trial_idx = start_range + ((end_range.to_f - start_range.to_f)/2).ceil
+            puts trial_idx
+            puts " #{sorted[trial_idx][0]} and #{find} #{start_range} #{end_range}"
+
+            if sorted[trial_idx][0] == find
+                result << sorted[trial_idx][0]
+                result << el[1]
+                break
+            elsif sorted[trial_idx][0] > find
+                end_range = trial_idx
+            else
+                start_range = trial_idx
             end
-            break if el[0] + sorted[sec_index][0] > target
         end
-        break if find == true
     end
-    return result
+    puts result
 end
 
 puts two_sum([2,7,11,15, 18, 21], 18);
