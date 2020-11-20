e508cbb8abdb84372ad2074807c64be8ed52a0bd
MarilenaRoque <marilenas@id.uff.br>
2020-11-19 23:29:07 -0300

    First Trial SumPairs solution
    Not passing all tests
    Fail when arrays get small

--- SumPairs.rb
+++ SumPairs.rb
@@ -10,14 +10,20 @@ def two_sum(nums, target)
         end_range = sorted.length - 1
         find = target - el[0]
 
-        while end_range - start_range > 1 do
+        while end_range > start_range do
 
-            trial_idx = start_range + ((end_range.to_f - start_range.to_f)/2).ceil
-            puts trial_idx
-            puts " #{sorted[trial_idx][0]} and #{find} #{start_range} #{end_range}"
-
+            trial_idx = start_range + ((end_range.to_f - start_range.to_f)/2).floor
+            if start_range + 1 == end_range
+                if sorted[end_range][0] == find
+                    result << sorted[end_range][1]
+                    result << el[1]
+                    break
+                else
+                    end_range = end_range - 1
+                end
+            end
             if sorted[trial_idx][0] == find
-                result << sorted[trial_idx][0]
+                result << sorted[trial_idx][1]
                 result << el[1]
                 break
             elsif sorted[trial_idx][0] > find
@@ -27,7 +33,7 @@ def two_sum(nums, target)
             end
         end
     end
-    puts result
+    return result
 end
 
 puts two_sum([2,7,11,15, 18, 21], 18);
