f219bf23562a53751961211303361f3372d807b5
MarilenaRoque <marilenas@id.uff.br>
2020-11-23 12:01:44 -0300

    Sum Pairs Solution

--- SumPairs.rb
+++ SumPairs.rb
@@ -1,36 +1,13 @@
 def two_sum(nums, target)
-    arr =[]
-    result =[]
-    nums.each_with_index do |item, idx|
-        arr << [item, idx]
-    end
-    sorted = arr.sort { |x,y| x[0] <=> y[0] }
-    sorted.each_with_index do |el, idx_first|
-        start_range = idx_first + 1
-        end_range = sorted.length - 1
-        find = target - el[0]
-
-        while end_range > start_range do
-
-            trial_idx = start_range + ((end_range.to_f - start_range.to_f)/2).floor
-            if start_range + 1 == end_range
-                if sorted[end_range][0] == find
-                    result << sorted[end_range][1]
-                    result << el[1]
-                    break
-                else
-                    end_range = end_range - 1
-                end
-            end
-            if sorted[trial_idx][0] == find
-                result << sorted[trial_idx][1]
-                result << el[1]
-                break
-            elsif sorted[trial_idx][0] > find
-                end_range = trial_idx
-            else
-                start_range = trial_idx
-            end
+    result = []
+    arr_new = nums
+    nums.each_with_index do |el, i|
+        rest = target - el
+        arr_new[i] = 'X'
+        found_idx = arr_new.index(rest)
+        if found_idx
+            result << i
+            result << found_idx
         end
     end
     return result
